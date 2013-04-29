a73271b2798c8b43aae5eced90fbd11a3eefbafb
louisbl <louis@hyperfiction.fr>
2013-04-29 14:43:50 +0200

    iOS: ARC and correct alloc of NSString. Remove unwanted files.

--- project/iPhone/HypGA.mm
+++ project/iPhone/HypGA.mm
@@ -10,47 +10,47 @@ namespace hypga {
 
 	void startNewSession( const char *sUID , int iPeriod ){
 		NSString *NSUID = [NSString stringWithUTF8String:sUID];
-		NSLog( @"startNewSession %d" , NSUID );
+		NSLog( @"startNewSession %@" , NSUID );
 		tracker = [[GAI sharedInstance] trackerWithTrackingId:NSUID];
 		//tracker.debug = YES;
 	}
 
 	void sendView( const char *sPage ){
-		NSString *NSPage = [[[NSString alloc] initWithUTF8String:sPage] autorelease];
+		NSString *NSPage = [[NSString alloc] initWithUTF8String:sPage];
 		NSLog( @"sendView %@" , NSPage );
 		[tracker sendView:NSPage];
 	}
 
 	void sendEvent( const char *sCat , const char *sAction , const char *sLabel , int iValue ){
-		NSString *NS_Cat = [[NSString stringWithUTF8String:sCat] autorelease];
-		NSString *NS_Act = [[NSString stringWithUTF8String:sAction] autorelease];
-		NSString *NS_Lab = [[NSString stringWithUTF8String:sLabel] autorelease];
+		NSString *NS_Cat = [ [NSString alloc] initWithUTF8String:sCat];
+		NSString *NS_Act = [ [NSString alloc] initWithUTF8String:sAction];
+		NSString *NS_Lab = [ [NSString alloc] initWithUTF8String:sLabel];
 		NSLog( @"SendEvent cat:%@ act:%@ label:%@ val%i" , NS_Cat , NS_Act , NS_Lab , iValue );
 		[tracker sendEventWithCategory:NS_Cat withAction:NS_Act withLabel:NS_Lab withValue:[NSNumber numberWithInt:iValue]];
 	}
 
 	void setCustom_dimension( int iIndex , const char *sValue ){
-		NSString *NS_Val = [[[NSString alloc] initWithUTF8String:sValue] autorelease];
-		NSLog( @"setCustom_dimension index:%i value:%@" , iIndex , sValue );
+		NSString *NS_Val = [[NSString alloc] initWithUTF8String:sValue];
+		NSLog( @"setCustom_dimension index:%i value:%s" , iIndex , sValue );
 		[tracker setCustom:iIndex dimension:NS_Val];
 	}
 
 	void setCustom_metric( int iIndex , int iMetric ){
-		NSLog( @"setCustom_metric index:%i metrid:%i");
+		NSLog( @"setCustom_metric index:%i metrid:%i",iIndex,iMetric);
 		[tracker setCustom:iIndex metric:[NSNumber numberWithInt:iMetric]];
 	}
 
 	void sendTiming( const char *sCat , int iInterval , const char *sName , const char *sLabel ){
-		NSString *NS_Cat	= [[[NSString alloc] initWithUTF8String:sCat] autorelease];
-		NSString *NS_Name	= [[[NSString alloc] initWithUTF8String:sName] autorelease];
-		NSString *NS_Label	= [[[NSString alloc] initWithUTF8String:sLabel] autorelease];
+		NSString *NS_Cat  	= [ [NSString alloc] initWithUTF8String:sCat];
+		NSString *NS_Name 	= [ [NSString alloc] initWithUTF8String:sName];
+		NSString *NS_Label	= [ [NSString alloc] initWithUTF8String:sLabel];
 		[tracker sendTimingWithCategory:NS_Cat withValue:iInterval withName:NS_Name withLabel:NS_Label];
 	}
 
 	void sendSocial( const char *sSocial_network , const char *sAction , const char *sTarget ){
-		NSString *NS_Net = [[NSString stringWithUTF8String:sSocial_network] autorelease];
-		NSString *NS_Act = [[NSString stringWithUTF8String:sAction] autorelease];
-		NSString *NS_Tgt = [[NSString stringWithUTF8String:sTarget] autorelease];
+		NSString *NS_Net = [ [NSString alloc] initWithUTF8String:sSocial_network];
+		NSString *NS_Act = [ [NSString alloc] initWithUTF8String:sAction];
+		NSString *NS_Tgt = [ [NSString alloc] initWithUTF8String:sTarget];
 		[tracker sendSocial:NS_Net withAction:NS_Act withTarget:NS_Tgt];
 	}
 
--- project/iphone.sh
+++ project/iphone.sh
@@ -1,18 +1,8 @@
 ﻿rm -rf "obj"
 echo "compiling for armv6"
-haxelib run hxcpp Build.xml -Diphoneos
+haxelib run hxcpp Build.xml -Diphoneos -DHXCPP_CLANG -DOBJC_ARC
 echo "compiling for armv7"
-haxelib run hxcpp Build.xml -Diphoneos -DHXCPP_ARMV7
+haxelib run hxcpp Build.xml -Diphoneos -DHXCPP_CLANG -DHXCPP_ARMV7  -DOBJC_ARC
 echo "compiling for simulator"
-haxelib run hxcpp Build.xml -Diphonesim
-echo "Copying sim"
-cp ../ndll/iPhone/libHypGA.iphonesim.a ../../../Export/ios/Test2/lib/i386/libHypGA.a
-echo "Copying sim debug"
-cp ../ndll/iPhone/libHypGA.iphonesim.a ../../../Export/ios/Test2/lib/i386-debug/libHypGA.a
-echo "Copying v6"
-cp ../ndll/iPhone/libHypGA.iphoneos.a ../../../Export/ios/Test2/lib/armv6/libHypGA.a
-echo "Copying v7"
-cp ../ndll/iPhone/libHypGA.iphoneos-v7.a ../../../Export/ios/Test2/lib/armv7/libHypGA.a
-echo "Copying v7 debug"
-cp ../ndll/iPhone/libHypGA.iphoneos-v7.a ../../../Export/ios/Test2/lib/armv7-debug/libHypGA.a
+haxelib run hxcpp Build.xml -Diphonesim -DHXCPP_CLANG  -DOBJC_ARC
 echo "Done !"
